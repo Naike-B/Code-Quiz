f7bfe03a71f1bc3d8cdc60139b52aea751bd6d98
Kiki <146773290+Naike-B@users.noreply.github.com>
2023-12-04 21:29:44 +0000

    Added functions to logic js file

---
+++ assets/js/logic.js
@@ -0,0 +1,95 @@
+// Variables 
+let secondsleft = 76; // variable to set the seconds for the timer
+let timeInterval; // variable for time interval
+let questionNumber; // variable to keep track of the current question in the quiz
+let score = 0; // variable to store the score
+
+// Variables for DOM manipulation
+const startButton = document.getElementById("start"); // start quiz button
+const startScreen = document.getElementById("start-screen"); // includes the heading the paragraph and the start quiz button
+const questions = document.getElementById("questions"); // questions div that contains the question title and answers buttons
+const timer = document.getElementById("time"); // time span
+const questionTitle = document.getElementById("question-title"); // question title includes the question 
+const answerButtons = document.getElementById("choices"); // div to include the answers buttons
+const feedback = document.getElementById("feedback"); // div to include feedback correct/wrong
+const endScreen = document.getElementById("end-screen"); // end screen
+const finalScore = document.getElementById("final-score"); // span to include final score
+const initials = document.getElementById("initials"); // input for initials
+const submitButton = document.getElementById("submit"); // submit button
+const highScores = document.getElementById("highscores"); // ol element from highscores html file
+
+// function to start quiz
+function startQuiz() {
+    startScreen.classList.add("hide"); // hides start screen
+    questions.classList.remove("hide"); // shows questions screen
+    questionNumber = 0; // is set to 0 so that the index 0 of the array is displayed first
+    // calls the function to start the timer 
+    setTime();
+    // Calls function to display question 
+    displayQuestion(quizQuestions[questionNumber]);
+}
+// Sets the timer and countdown and displays time
+function setTime() {
+     timeInterval = setInterval(function () {
+        secondsleft--;
+        timer.textContent = secondsleft; 
+    }, 1000);
+}
+
+startButton.addEventListener("click", startQuiz); // If user clicks the start button the startQuiz function is triggered
+
+// Function to display questions
+function displayQuestion() {
+    const currentQuestion = quizQuestions[questionNumber]; // sets variable currentQuestion to be a q from array based on q number
+    questionTitle.innerText = currentQuestion.question; // innerText returns the text content of question
+    answerButtons.innerHTML = ""; // clears content of answerButtons element to remove previously displayed answer buttons before new ones are added
+    currentQuestion.answers.forEach(function (answer, i) { // forEach iterates over the answers array. for each iteration it passes the current element to the callback function as the answer parameter
+        const button = document.createElement("button"); // method to create a new button element
+        button.innerText = answer.text; // innerText sets the text of the button as the answer 
+        button.dataset.index = i; // sets the data attribute for button; the value of the attribute is set to i (i = index of the current answer in the answers array)
+        button.addEventListener("click", checkAnswer); // every button has an event listener that triggers function checkAnswer 
+        answerButtons.appendChild(button); // appendChild adds the button to the body of the document   
+    });
+}
+
+// Function to check the answer 
+function checkAnswer(event) {
+    const currentQuestion = quizQuestions[questionNumber]; // // sets variable currentQuestion to be a q from array based on q number
+    const answerIndex = event.target.dataset.index; // when button is clicked grab answer index from answers array in question object
+    const answer = currentQuestion.answers[answerIndex]; // variable answer is the answers of the current question 
+    if (answer.correct) { // if answer correct is true
+        score+= 5; // adds 5 point to score
+        feedback.textContent = "Correct"; // displays Correct
+        const audio = new Audio("./assets/sfx/correct.wav"); // stores audio for correct answer
+        audio.play(); // play method to play sound
+    } else { // if false
+        secondsleft -= 10; // deducts 10seconds from timer
+        feedback.textContent = "Wrong"; // displays Wrong
+        const audio = new Audio("./assets/sfx/incorrect.wav"); // stores audio for incorrect answer
+        audio.play(); // play method to play sound
+    };
+    questionNumber++; // go to next question
+    if (questionNumber < quizQuestions.length) {  // if there is a question in the array
+    displayQuestion(quizQuestions[questionNumber]); // display question
+    }
+
+    endQuiz(); // checks if the quiz should end
+}
+
+// Function to end quiz when time reached 0 or all questions are answered
+function endQuiz() {
+    if (secondsleft <= 0 || questionNumber >= quizQuestions.length) {
+        clearInterval(timeInterval); // clears timer
+        startScreen.classList.add("hide"); // hides start screen
+        questions.classList.add("hide"); // hides question screen
+        feedback.classList.add("hide"); // hides feedback
+        endScreen.classList.remove("hide"); // shows end screen
+        finalScore.textContent = score; // changes final score element to display score
+        console.log(questionNumber);
+        console.log(quizQuestions.length);
+        console.log(score);
+    };
+}
+
+// session storage 
+
---
+++ assets/js/questions.js
@@ -0,0 +1,110 @@
+let quizQuestions = [
+    {
+        question: "In JavaScript, what element is used to store multiple values in a single variable?", 
+        answers: [
+            { text: "Variables", correct: false },
+            { text: "Strings", correct: false },
+            { text: "Arrays", correct: true },
+            { text: "Functions", correct: false }
+        ]
+    },
+    {
+        question: "What are the identifiers called, that cannot be used as variables or function names?",
+        answers: [
+            { text: "Favorites", correct: false },
+            { text: "Concrete Terms", correct: false },
+            { text: "Reserved Words", correct: false },
+            { text: "Constants", correct: true }
+        ]
+    },
+    {
+        question: "What is the default behavior called that is used to move declarations to the top of the current scope?", 
+        answers: [
+            { text: "Jumping", correct: false },
+            { text: "Hoisting", correct: true },
+            { text: "Arranging", correct: false },
+            { text: "Sorting", correct: false }
+        ]
+    },
+    {
+        question: "What is the element called that forms a search pattern out of a sequence of characters?", 
+        answers: [
+            { text: "Coditional Argument", correct: true },
+            { text: "Boolean Variable", correct: false },
+            { text: "RegExp or Regular Expression", correct: false },
+            { text: "Event", correct: false }
+        ]
+    },
+    {
+        question: "What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?",
+        answers: [
+            { text: "Repeater", correct: false },
+            { text: "Clone", correct: false },
+            { text: "Loop", correct: true },
+            { text: "Debugger", correct: false }
+        ]
+    },
+    {
+        question: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+        answers: [
+            { text: "Function", correct: true },
+            { text: "Variable", correct: false },
+            { text: "Declaration", correct: false },
+            { text: "String", correct: false }
+        ]
+    },
+    {
+        question: "What kind of statement is used to execute actions based on a trigger or condition?", 
+        answers: [
+            { text: "Boolean Variable", correct: false },
+            { text: "Fired Event", correct: true },
+            { text: "RegExp or Regular Expression", correct: false },
+            { text: "Conditional Statement", correct: false }
+        ]
+    },
+    {
+        question: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?", 
+        answers: [
+            { text: "Output", correct: false },
+            { text: "JSON", correct: false },
+            { text: "Scope", correct: false },
+            { text: "Syntax", correct: true }
+        ]
+    },
+    {
+        question: "What is the object called that lets you work with both dates and time-related data?", 
+        answers: [
+            { text: "Time-warp", correct: false },
+            { text: "Dates", correct: false },
+            { text: "Clock", correct: true },
+            { text: "Time field", correct: false }
+        ]
+    },
+    {
+        question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?", 
+        answers: [
+            { text: "Scope", correct: true },
+            { text: "Range", correct: false },
+            { text: "Output Level", correct: false },
+            { text: "Restrictions", correct: false }
+        ]
+    },
+    {
+        question: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?", 
+        answers: [
+            { text: "For Loop", correct: false },
+            { text: "Else Loop", correct: false },
+            { text: "Conditional Loop", correct: false },
+            { text: "While Loop", correct: true }
+        ]
+    },
+    {
+        question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?", 
+        answers: [
+            { text: "Variables", correct: false },
+            { text: "Recorders", correct: false },
+            { text: "Strings", correct: true },
+            { text: "Arrays", correct: false }
+        ]
+    }
+];
